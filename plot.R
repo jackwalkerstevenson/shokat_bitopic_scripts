54ddc2081b6b4a98d04a9779f144055d15b72e3a
kajabla <jskestrel@gmail.com>
2022-04-25 15:29:07 -0700

    Updated readme text and got dose-response curves to work

--- plot.R
+++ plot.R
@@ -1,11 +1,11 @@
-#'This is the R script that is used to generate nice plots from plate data.
-#'Assumes a 96-well plate in which the outer border of cells are not read
-#'Assumes the format output by Tecan Spark:
-#'"<>" appears at the corner of row and column names
+#'plateplotr generates plots with dose-response curves from 96-well plate data.
+#'input: data in plater format for tidy import
+#'copy raw plate reader data into JWS Excel template and fill in input cells
+#'save input file as csv for import
+library(drc)  # for dose response curves
 library(tidyverse)
-library(readxl)
-library(plater)
-library(drc)
+library(ggprism)  # for pretty prism-like plots
+library(plater)  # for plate data import
 # set up input parameters
 # input_filename = "JS-B2-79 CTG 1.xlsx"
 # reference_symbol = "<>"  # symbol that appears right before plate data
@@ -23,18 +23,22 @@ library(drc)
 #   select(3:12)
 plater_data <- read_plate("JS-B2-79 plater test.csv") %>%
   filter(drug != "N/A") %>%
+  # drop 0 values for plotting and curve fitting
+  # note this is only OK because normalization is happening in Excel
+  filter(concentration != 0) %>%
   mutate(log.conc = log10(concentration))
 plater_data.summary <- plater_data %>%
     group_by(cell_line, concentration) %>%
     summarize(
       sem = sd(CTG_normalized, na.rm = TRUE)/sqrt(n()),
       CTG_normalized = mean(CTG_normalized),
-      log.conc = log.conc
+      log.conc = log.conc,
+      drug = drug
     )
 
 plater_data.summary %>% ggplot(aes(x = log.conc, y = CTG_normalized, color = cell_line))+
     geom_point()+
     geom_errorbar(aes(ymax = CTG_normalized+sem, ymin = CTG_normalized-sem))+
-    geom_smooth(method = "drm", fct = L.4(), se = FALSE)
-                
-
+    geom_smooth(method = "drm", method.args = list(fct = L.4()), se = FALSE)+
+    theme_prism()+
+    labs(x = "Log [compound] (µM)", y = "Relative cell viability (%)")
