c94373d5571698ade171132d16f95ca891f199da
kajabla <jskestrel@gmail.com>
2022-04-27 21:32:50 -0700

    last commit didn't seem to capture many changes to script, for some reason. same changes, better comments. also updating example import files and plate template

--- plot.R
+++ plot.R
@@ -1,52 +1,87 @@
-#'plateplotr generates plots with dose-response curves from 96-well plate data.
-#'input: data in plater format for tidy import
-#'copy raw plate reader data into JWS Excel template and fill in input cells
-#'save input file as csv for import
+#' ---
+#'title: "plateplotr"
+#'author: "Jack Stevenson"
+#'date: "2022-04-26"
+#' ---
+#'plateplotr generates dose-response plots from plate reader data.
+#' 
+#'Input: data formatted as per the plater package. Use the JWS Excel plater template to make this easy.
+#' 
+#'Variables expected in the import file (JWS Excel template is already set up for this):
+#' 
+#'- 'compound': name of compound used (including DMSO wells in dilution series)
+#'- 'conc': concentration of compound used
+#'- 'cell_line': cell line treated
+#'- 'readout': raw plate reader data (this is only used to calculate read_norm)
+#'- 'read_norm': normalized plate reader data (calculated in the spreadsheet)
+#'- 'replicate': replicate of curve (not actually used)
+#'
+#'How to use plateplotr:
+#'
+#'1. Make a copy of the JWS Excel plater template for each plate you want to import
+#'2. Fill out each copy with the compound + concs used and paste in raw plate reader data
+#'3. Save input sheets as csv and put them in your working directory (or move your working directory to where they are)
+#'  - alternatively, manually create plater-formatted csv sheets and put them in the working directory
+#'4. Fill out input filenames in plate_files below
+#'5. Run plot.R (this file)
+#'6. Output plots will be created in a "plots" folder in the working directory
+
+# Load required libraries------------------------------------------------------
 library(drc)  # for dose response curves
-library(tidyverse)
+library(tidyverse) # for tidy data handling
 library(ggprism)  # for pretty prism-like plots
-library(viridis)
-library(plater)  # for plate data import
+library(plater)  # for tidy importing of plate data
+
+# specify names of input files and import data---------------------------------
 plate_files = c("JS-B2-79 plater 1.csv",
                 "JS-B2-79 plater 2.csv",
                 "JS-B2-79 plater 3.csv",
                 "JS-B2-79 plater 4.csv",
                 "JS-B2-79 plater 5.csv")
-plate_names = c("Dasatinib",
-                "DasatiLink-1",
-                "Ponatinib",
-                "PonatiLink-1",
-                "Asciminib")
-plater_data <- read_plates(plate_files, plate_names) %>%
-  filter(drug != "N/A") %>%
-  # drop 0 values for plotting and curve fitting
-  # note this is only OK because normalization is happening in Excel
-  filter(concentration != 0) %>%
-  mutate(log.conc = log10(concentration/1e6))  # convert conc from µM to M
+plate_names = seq(1,length(plate_files))  # create plate IDs
+plate_data <- read_plates(plate_files, plate_names) %>%
+  # drop empty wells
+  filter(compound != "N/A") %>%
+  # drop 0 values before plotting and curve fitting
+  # note this is only OK because normalization happens before import
+  filter(conc != 0) %>%
+  mutate(log.conc = log10(conc/1e6))  # convert conc from µM to M
 
-for(p in distinct(plater_data["Plate"])$Plate){
-  print(str_glue("working on plate {p}"))
-  plate.summary <- plater_data %>%
-    filter(Plate == p) %>%
-    group_by(cell_line, concentration) %>%
+# analyze and plot data for each compound--------------------------------------
+for (cpd in distinct(plate_data["compound"])$compound){
+  print(str_glue("working on compound: {cpd}"))
+  plate.summary <- plate_data %>%
+    filter(compound == cpd) %>% # get data from one compound to work with
+    group_by(cell_line, conc) %>%  # get set of replicates for each condition
+    # create summary table for plotting
     summarize(
-      sem = sd(CTG_normalized, na.rm = TRUE)/sqrt(n()),
-      CTG_normalized = mean(CTG_normalized),
+      # standard error for error bars = standard deviation / square root of n
+      sem = sd(read_norm, na.rm = TRUE)/sqrt(n()),
+      # get mean normalized readout value for plotting
+      mean_read = mean(read_norm),
+      # carry concentration through for plotting
       log.conc = log.conc
-    )
-  print(str_glue("Done creating summary for plate {p}"))
-  plate.summary %>% ggplot(aes(x = log.conc, y = CTG_normalized, color = cell_line))+
-    geom_point()+
-    geom_errorbar(aes(ymax = CTG_normalized+sem, ymin = CTG_normalized-sem))+
-    geom_smooth(method = "drm", method.args = list(fct = L.4()), se = FALSE)+
-    theme_prism()+
-    scale_color_manual(values = c("darkred","black"))+
-    scale_x_continuous(limits = c(-10,-5))+
-    scale_y_continuous(breaks = c(0,25,50,75,100),
-                       limits = c(0,NA))+
-    labs(x = "Log [compound] (M)",
-         y = "Relative cell viability (%)",
-         title = p)
-  ggsave(str_glue("plots/{p}.pdf"), width = 5, height = 4)
-  print(str_glue("done plotting plate {p}"))
+      ) %>%
+    # todo: factors so cell lines get plotted and colored in input order
+    mutate(cell_line = fct_relevel(cell_line, c("K562 wt", "K562 T315I")))
+
+  # plot data and fit dose response curve
+  plate.summary %>%
+    ggplot(aes(x = log.conc, y = mean_read, color = cell_line))+
+      geom_point()+
+      # error bars = mean plus or minus standard error
+      geom_errorbar(aes(ymax = mean_read+sem, ymin = mean_read-sem))+
+      # use drm method from drc package to fit dose response curve
+      geom_smooth(method = "drm", method.args = list(fct = L.4()), se = FALSE)+
+      theme_prism()+ # make it look like prism
+      scale_color_manual(values = c("darkred","black"))+
+      scale_x_continuous(limits = c(-10,-5))+
+      scale_y_continuous(breaks = c(0,25,50,75,100),
+                         limits = c(0,NA))+
+      labs(x = "Log [compound] (M)",
+           y = "Relative cell viability (%)",
+           title = cpd)
+  # save plot with manually optimized aspect ratio
+  ggsave(str_glue("plots/{cpd}.pdf"), width = 5, height = 4)
+  print(str_glue("done plotting compound {cpd}"))
   }
