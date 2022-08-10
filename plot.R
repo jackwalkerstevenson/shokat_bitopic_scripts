58302dab5e25d467600785348d7072fd86cdd665
kajabla <jskestrel@gmail.com>
2022-08-09 22:32:45 -0700

    Fixed error bar width bug, lots of small plot cleanup, successfully plotting all data for each cell line

--- plot.R
+++ plot.R
@@ -1,7 +1,7 @@
 #' ---
 #'title: "plateplotr"
 #'author: "Jack Stevenson"
-#'date: "2022-07-01"
+#'date: "2022-08-09"
 #' ---
 #'plateplotr generates dose-response plots from plate reader data.
 #' 
@@ -18,13 +18,13 @@
 #'
 #'How to use plateplotr:
 #'
-#'1. Make a copy of the JWS Excel plater template for each plate you want to import
+#'1. Make one copy of the JWS Excel plater template for each plate of data you want to import
 #'2. Fill out each copy with the compound + concs used and paste in raw plate reader data
 #'3. Save input sheets as csv and put them in your working directory (or move your working directory to where they are)
-#'  - alternatively, manually create plater-formatted csv sheets and put them in the working directory
-#'4. Fill out input filenames in plate_files below
+#'  - alternatively, manually create plater-formatted csv files and put them in the working directory
+#'4. Fill out input filenames in the plate_files variable below
 #'5. Run plot.R (this file)
-#'6. Output plots will be created in a "plots" folder in the working directory
+#'6. Output plots will be created in a "plots output" folder in the working directory
 
 # Load required libraries------------------------------------------------------
 library(drc)  # for dose response curves
@@ -57,89 +57,82 @@ plate_data <- plate_data %>%
   mutate(cell_line = fct_relevel(cell_line, cell_line_factors)) %>%
   mutate(compound = fct_relevel(compound, compound_factors))
 
-# automatically determine x-axis limits for consistent limits between compounds
+# find x-axis min/max values for consistent zoom window between all plots
 x_limits <- c(floor(min(plate_data$log.conc)), ceiling(max(plate_data$log.conc)))
-# x_limits <- c(NA,NA) # Manual override of x-axis limits
 
 # plot data for each compound--------------------------------------
 for (cpd in distinct(plate_data["compound"])$compound){
   print(str_glue("working on compound {cpd}"))
-  plate.summary <- plate_data %>%
+  plate_data %>%
     filter(compound == cpd) %>% # get data from one compound to work with
-    group_by(cell_line, conc) %>%  # get set of replicates for each condition
+    group_by(cell_line, log.conc) %>%  # get set of replicates for each condition
     # create summary table for plotting
     summarize(
       # standard error for error bars = standard deviation / square root of n
       sem = sd(read_norm, na.rm = TRUE)/sqrt(n()),
       # get mean normalized readout value for plotting
       mean_read = mean(read_norm),
-      # carry concentration through for plotting
-      log.conc = log.conc
-      )
-
-  # plot data and fit dose response curve
-  plate.summary %>%
-    ggplot(aes(x = log.conc, y = mean_read, color = cell_line))+
-      geom_point()+
+      w = 0.1 * n() # necessary for consistent error bar widths across plots
+      ) %>%
+    ggplot(aes(x = log.conc, y = mean_read, color = cell_line)) +
+      geom_point() +
       # error bars = mean plus or minus standard error
-      geom_errorbar(aes(ymax = mean_read+sem, ymin = mean_read-sem))+
+      geom_errorbar(aes(ymax = mean_read+sem, ymin = mean_read-sem, width = w)) +
       # use drm method from drc package to fit dose response curve
-      geom_smooth(method = "drm", method.args = list(fct = L.4()), se = FALSE)+
-      scale_color_manual(values = c("black","darkred"))+
-      # set axis ticks
-      scale_x_continuous(breaks = ) +
-      scale_y_continuous(breaks = c(0,25,50,75,100)) +
-      # set axis limits from global values
-      coord_cartesian(xlim = x_limits,
-                      ylim = c(0,NA)) +
-      theme_prism()+ # make it look like prism
-      theme(plot.background = element_blank()) +
+      geom_line(stat = "smooth", method = "drm", method.args = list(fct = L.4()),
+                se = FALSE, size = 1) +
+      scale_color_manual(values = c("black","darkred")) +
+      scale_x_continuous() + # automatic x axis ticks
+      scale_y_continuous(breaks = c(0,25,50,75,100)) + # manual y axis ticks
+      coord_cartesian(xlim = x_limits, # set x axis zoom from global values
+                      ylim = c(0,NA)) + # set y axis zoom locally
+      theme_prism() + # make it look fancy like prism
+      theme(plot.background = element_blank()) + # need for transparent background
       labs(x = "Log [compound] (M)",
            y = "Relative cell viability (%)",
            title = cpd)
-  
   # save plot with manually optimized aspect ratio
   ggsave(str_glue("plots output/{cpd}.pdf"), width = 5, height = 4, bg = "transparent")
   print(str_glue("done plotting compound {cpd}"))
 }
-# plot data for each cell line (currently error bar problem)-------------------------------------------------
-alpha_val <- 1
+# plot data for each cell line-------------------------------------------------
+alpha_val <- .7
 mako_start <- 0.9
 mako_end <- 0.3
 viridis_start <- 1
 viridis_end <- 0
+compounds_to_plot <- c("asciminib", "ponatinib")
 for (c_line in distinct(plate_data["cell_line"])$cell_line){
   print(str_glue("working on cell line {c_line}"))
   plate_data %>%
     filter(cell_line == c_line) %>%
-    group_by(compound, conc) %>% # group into replicates for each condition
+    #filter(compound %in% compounds_to_plot) %>% # for testing asciminib error bar bug
+    group_by(compound, log.conc) %>% # group into replicates for each condition
     summarize(
       # standard error for error bars = standard deviation / square root of n
       sem = sd(read_norm, na.rm = TRUE)/sqrt(n()),
-      # get mean normalized readout value for plotting
-      mean_read = mean(read_norm),
-      # carry concentration through for plotting
-      log.conc = log.conc) %>%
-    ggplot(aes(x = log.conc, y = mean_read, color = compound))+
-    geom_point()+
+      mean_read = mean(read_norm), # mean normalized readout value for plotting
+      w = 0.1 * n() # necessary for consistent error bar widths across plots
+      ) %>%
+    # todo: both cell lines on one plot, linetype and shape for cell line
+    ggplot(aes(x = log.conc, y = mean_read, color = compound)) +
+    geom_point() +
     # error bars = mean plus or minus standard error
-    geom_errorbar(aes(ymax = mean_read+sem, ymin = mean_read-sem))+
+    geom_errorbar(aes(ymax = mean_read+sem, ymin = mean_read-sem, width = w)) +
     # use drm method from drc package to fit dose response curve
-    geom_smooth(method = "drm", method.args = list(fct = L.4()), se = FALSE)+
-    #this is what you have to do to get alpha on the lines, but then they are thin
-    #stat_smooth(geom = 'line',method = "drm", method.args = list(fct = L.4()), se = FALSE)+ 
-    # set axis ticks
-    scale_x_continuous() +
-    scale_y_continuous(breaks = c(0,25,50,75,100)) +
-    coord_cartesian(xlim = x_limits, # set x axis limits from global values
-                    ylim = c(0,NA)) + # set y axis limit locally
-    theme_prism()+ # make it look fancy like prism
-    theme(plot.background = element_blank())+ # need for transparent background
-    scale_color_viridis(option = "viridis", discrete = TRUE, begin = viridis_start, end = viridis_end)+
+    geom_line(stat = "smooth", method = "drm", method.args = list(fct = L.4()),
+              se = FALSE, size = 1, alpha = alpha_val) +
+    scale_x_continuous() + # automatic x axis ticks
+    scale_y_continuous(breaks = c(0,25,50,75,100)) + # manual y axis ticks
+    coord_cartesian(xlim = x_limits, # set x axis zoom from global values
+                    ylim = c(0,NA)) + # set y axis zoom locally
+    theme_prism() + # make it look fancy like prism
+    theme(plot.background = element_blank()) + # need for transparent background
+    scale_color_viridis(option = "viridis", discrete = TRUE, begin = viridis_start, end = viridis_end) +
     #scale_color_viridis(option = "mako", discrete = TRUE, begin = mako_start, end = mako_end)+
-    labs(x = "Log [compound] (M)",
-         y = "Relative cell viability (%)",
+    labs(x = "log [compound] (M)",
+         y = "relative cell viability (%)",
          title = c_line)
-  ggsave(str_glue("plots output/{c_line}.pdf"), width = 6, height = 4, bg = "transparent")
+  ggsave(str_glue("plots output/{c_line}.pdf"), width = 7, height = 5, bg = "transparent")
   print(str_glue("done plotting cell line {c_line}"))
 }
