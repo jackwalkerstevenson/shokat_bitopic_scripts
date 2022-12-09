0f81bc4bde066e45ef2b67f0977c82013c927f20
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2022-12-08 17:48:26 -0800

    Many small changes. EC50 plotting now pretty good!

--- EC50_plot.R
+++ EC50_plot.R
@@ -24,23 +24,26 @@ compounds <- c("ponatinib",
                "PonatiLink-1-20",
                "PonatiLink-1-24")
 EC_data <- read_csv(input_filename) %>%
-  mutate(pEC50_nM = -log10(EC50_nM)) %>%
+  mutate(log10EC50_nM = log10(EC50_nM)) %>%
   # filter for desired compounds
   filter(compound %in% compounds) %>%
   mutate(compound = fct_relevel(compound, compounds)) # order compounds by list
 # set factors so experiments and targets get plotted and colored in input order
 experiment_factors <- distinct(EC_data, experiment)$experiment
-target_factors <- distinct(EC_data, target)$target
+Abl_factors <- distinct(EC_data, Abl)$Abl
 EC_data <- EC_data %>% 
   mutate(experiment = fct_relevel(experiment, experiment_factors)) %>%
-  mutate(target = fct_relevel(target, target_factors))
+  mutate(Abl = fct_relevel(Abl, Abl_factors))
 # plot ECs in points---------------------------------------------------------------
 EC_data %>%
-  ggplot(aes(x = compound, y = pEC50_nM)) +
+  ggplot(aes(x = compound, y = log10EC50_nM)) +
   geom_point(aes(shape = target, color = experiment), size = 3) +
   scale_x_discrete(guide = guide_axis(angle = -90)) +
   theme_prism() + # make it look fancy like prism
   scale_color_viridis(discrete = TRUE, begin = 0.3, end = 0.8) +
+  labs(x = "compound",
+       y = "log10(EC50) (nM)",
+       title = "Relative potency of compounds") +
   theme(plot.background = element_blank()) # need for transparent background
   ggsave(str_glue("plots output/EC50_points.{plot_type}"),
             bg = "transparent",
@@ -50,20 +53,23 @@ EC_data %>%
 # plot ECs by linker length-----------------------------------------------------
 EC_data %>%
   filter(linker_length > 0) %>%
-  #filter(experiment == "CTG") %>%
-  group_by(experiment) %>%
-  #filter(target == "wt") %>%
-  ggplot(aes(x = linker_length, y = pEC50_nM,
-             color = experiment, shape = target)) +
+  #group_by(experiment) %>%
+  ggplot(aes(x = linker_length, y = EC50_nM,
+             color = experiment, shape = Abl)) +
   scale_x_continuous(guide = "prism_offset_minor", # end at last tick
                      breaks = c(12, 16, 20, 24)) + # manual x ticks
+  scale_y_log10(guide = "prism_offset") +
+  #scale_y_continuous(guide = "prism_offset") +
+  scale_color_manual(values = c("blue2","green3"), labels = c("CellTiter-Glo", "SelectScreen")) +
+  #scale_color_viridis(discrete = TRUE, begin = 0.3, end = 0.8) +
+  guides(color=guide_legend(override.aes=list(shape=15))) +
   geom_point(size = 3) +
   geom_line() +
   theme_prism() + # make it look fancy like prism
-  scale_color_viridis(discrete = TRUE, begin = 0.3, end = 0.8) +
   theme(plot.background = element_blank()) + # need for transparent background
-  labs(x = "linker length",
-       y = "pEC50 (nM)")
+  labs(x = "linker length (PEG units)",
+       y = "EC50 (nM)",
+       title = "Potency of PonatiLink-1 series by linker length") +
   ggsave(str_glue("plots output/EC50_linker.{plot_type}"),
          bg = "transparent",
          width = 7,
