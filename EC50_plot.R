01b0f28e0b2b424385caa7f124b924c5f9d99087
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-02-28 12:55:02 -0800

    Moved manual scales into accessory keys file and slightly reflowed linker data

--- EC50_plot.R
+++ EC50_plot.R
@@ -11,15 +11,16 @@ library(scales)
 library(ggprism)  # for pretty prism-like plots
 library(viridis) # for color schemes
 library(lemon) # for fancy facet wrapping
-# specify names of input files and import data---------------------------------
+# import parameter files and data---------------------------------
 # note the order compounds are imported is the order they will be plotted
 input_filename <- "EC50s.csv"
 dir.create("output/", showWarnings = FALSE)
 plot_type <- "pdf"
-# choose and order compounds and targets to plot
+# import and order compounds and targets to plot
 source("compounds.R")
 source("targets.R")
 source("variants.R")
+source("keys.R")
 EC_data <- read_csv(input_filename) %>%
   mutate(linker_length = as.numeric(linker_length)) %>%
   mutate(EC50_nM = as.numeric(EC50_nM)) %>%
@@ -35,13 +36,12 @@ EC_data <- read_csv(input_filename) %>%
 EC_data %>%
   ggplot(aes(x = compound, y = EC50_nM)) +
   geom_point(aes(shape = assay, color = variant), size = 4, alpha = 1) +
-  scale_shape(labels = c("CellTiter-Glo, K562", "SelectScreen, Abl1"),
+  scale_shape(labels = shape_labels,
               guide = guide_legend(order = 1)) + # force to top of legend
   scale_x_discrete(guide = guide_axis(angle = -90)) +
-  # -log10 transform to show most potent on top
+  # reverse and log10 transform to show most potent on top
   scale_y_continuous(trans = c("log10","reverse")) +
-  scale_color_manual(values = c("black","red3","blue2"),
-                     labels = c("Abl wt", "Abl T315I", "Abl V468F")) +
+  scale_color_manual(values = variant_colors) +
   theme_prism() + # make it look fancy like prism
   labs(x = "compound",
        y = "EC50 (nM) [more potent ->]",
@@ -56,7 +56,7 @@ EC_data %>%
   filter(linker_length > 0) %>% # only plot compounds with linkers
   ggplot(aes(x = linker_length, y = EC50_nM,
              shape = assay, color = variant)) +
-  scale_shape(labels = c("CellTiter-Glo, K562", "SelectScreen, Abl1"),
+  scale_shape(labels = shape_labels,
               guide = guide_legend(order = 1)) + # force to top of legend
   theme_prism() + # make it look fancy like prism
   scale_x_continuous(
@@ -64,9 +64,7 @@ EC_data %>%
                      breaks = seq(11,23,2)) + # manual x ticks
   scale_y_continuous(trans = c("log10", "reverse"),
                      guide = "prism_offset_minor") +
-  scale_color_manual(values = c("black","red3","blue2"),
-                     labels = c("Abl wt", "Abl T315I", "Abl V468F")) +
-  #scale_color_viridis(discrete = TRUE, begin = 0.3, end = 0.8) +
+  scale_color_manual(values = variant_colors) +
   # remove placeholder shape from color legend with 32, the nonshape
   guides(color=guide_legend(override.aes=list(shape=32))) +
   geom_point(size = 4) +
@@ -80,22 +78,24 @@ ggsave(str_glue("output/EC50_linker.{plot_type}"),
        width = 8,
        height = 4)
 # prepare data to plot ECs across assays----------------------------------------
-linker_data <- EC_data %>%
-  pivot_wider(names_from = assay, values_from = EC50_nM, id_cols = c(linker_length, variant)) %>%
-  mutate(variant = recode(variant, "wt" = "Abl wt", "T315I" = "Abl T315I")) %>%
-  filter(linker_length > 0) # only plot compounds with linkers
 linker_min <- min(linker_data$linker_length)
 linker_max <- max(linker_data$linker_length)
 linker_seq <- seq(linker_min, linker_max, 2)
 # plot ECs across assays--------------------------------------------------------
-linker_data %>%
+EC_data %>%
+  # pivot data so that EC50s from each assay associate with the same linker/variant combo
+  pivot_wider(names_from = assay,
+              values_from = EC50_nM,
+              id_cols = c(linker_length, variant)) %>%
+  filter(linker_length > 0) %>% # only plot compounds with linkers
   ggplot(aes(x = CTG, y = SelectScreen)) +
   theme_prism() + # make it look fancy like prism
   lemon::facet_rep_wrap(vars(variant), repeat.tick.labels = "left") + # repeat y axis
   theme(panel.spacing = unit(.5, "inches")) +
   scale_x_continuous(trans = c("log10", "reverse")) +
-  scale_y_continuous(trans = c("log10", "reverse"),
-                     breaks = c(1.5, 1, .5)) +
+  scale_y_continuous(trans = c("log10", "reverse")
+                     #breaks = c(1.5, 1, .5)
+                     ) +
   coord_fixed() + # even coordinate spacing on both axes
   geom_point(aes(size = linker_length, color = linker_length)) +
   # specify manual limits, breaks and labels so 
