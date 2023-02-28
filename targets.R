742aad6f9bdd17e673e6ce42f452d7b4fbe14611
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-02-27 16:15:14 -0800

    Added more targets

--- targets.R
+++ targets.R
@@ -1,7 +1,12 @@
 targets <- c(
   "K562 wt",
+  # "K562 pUltra BCR-ABL wt",
   "K562 T315I",
+  "K562 pUltra BCR-ABL V468F",
   "ABL1",
   "ABL1 T315I"
+  # "FGFR1",
+  # "FLT1 (VEGFR1)",
+  # "PDGFRA (PDGFR alpha)"
   # "K562 pUltra BCR-ABL V468F"
 )
