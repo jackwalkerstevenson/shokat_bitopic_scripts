4b5efe9ff00bbd853cdc2cda4a15fc98cc831c24
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-01 00:38:31 -0800

    Moved plot_global function into separate file

---
+++ plot_global.R
@@ -0,0 +1,13 @@
+plot_global <- function(plot){
+  plot +
+    scale_x_continuous(guide = "prism_offset_minor", # end at last tick
+                       breaks = breaks_width(1),
+                       minor_breaks = minor_x) + # manual minor ticks
+    scale_y_continuous(guide = "prism_offset",  # end at last tick
+                       breaks = c(0,25,50,75,100)) + # manual y axis ticks
+    coord_cartesian(xlim = x_limits, # set x axis zoom from global values
+                    ylim = c(0,NA)) + # set y axis zoom locally
+    theme_prism() + # make it look fancy like prism
+    theme(plot.background = element_blank()) + # need for transparent background
+    labs(x = "log [compound] (M)")
+}
