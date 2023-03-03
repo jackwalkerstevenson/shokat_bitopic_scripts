959e1b398b15d1a3f1cb4d5d2ce9819c5068bf7f
kajabla <jskestrel@gmail.com>
2023-03-02 18:32:47 -0800

    moved parameters to new parameters directory and added keys file

---
+++ parameters/keys.R
@@ -0,0 +1,9 @@
+assay_labels <- c(
+  "CTG" = "CellTiter-Glo, K562",
+  "SelectScreen" = "Thermo SelectScreen, Abl1"
+)
+variant_colors <- c(
+  "wt" = "black",
+  "T315I" = "red3",
+  "V468F" = "blue2"
+)
