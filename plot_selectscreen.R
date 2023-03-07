ebc71b66efcbb4bb24fd85d61399069c5bd87a30
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-06 20:25:17 -0800

    Rejiggered plot_selectscreen to use external import function

--- plot_selectscreen.R
+++ plot_selectscreen.R
@@ -22,21 +22,8 @@ dir.create("output/", showWarnings = FALSE) # silently create output directory
 source("parameters/compounds.R")
 source("parameters/targets.R")
 # import and tidy data---------------------------------
-# note the order compounds are imported is the order they will be plotted
-plate_data <- read_csv(input_filename) %>%
-  # rename relevant columns to common names
-  rename(compound = Compound) %>%
-  rename(target = Kinase) %>%
-  # filter for desired compounds and targets and order by input lists
-  filter(compound %in% compounds) %>%
-  mutate(compound = fct_relevel(compound, compounds)) %>%
-  filter(target %in% targets) %>%
-  mutate(target = fct_relevel(target, targets)) %>%
-  # tidy by pivoting duplicates to one measurement per row
-  pivot_longer(cols = c(pct_Inhibition_1, pct_Inhibition_2), names_to = NULL, values_to = "pct_inhibition") %>%
-  # wrangle: convert conc to log molar and convert percent inhibition to activity
-  mutate(log.conc = log10(Compound_Conc_nM/1e9)) %>%
-  mutate(activity = 100 - pct_inhibition)
+source("import_selectscreen.R")
+plate_data <- import_selectscreen(input_filename, compounds, targets)
 # fit models to output EC values------------------------------------------------
 # seems like you should be able to just pipe group_by into drm(), but nope, so doing this instead
 # helper function for getting EC for one compound, cell line, and EC threshold
