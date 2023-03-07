c40bcecc467c3652237e46b0c131d1d1d4c23959
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-06 20:41:24 -0800

    Added arguments to selectscreen import function: now can filter for compounds and targets on demand with sensible defaults

--- import_selectscreen.R
+++ import_selectscreen.R
@@ -1,15 +1,26 @@
-import_selectscreen <- function(input_filename, compounds, targets){
-  read_csv(input_filename) %>%
+import_selectscreen <- function(input_filename, compounds, targets, filter = !(missing(compounds) & missing(targets))){
+  data <- read_csv(input_filename) %>%
     # rename relevant columns to common names
     rename(target = Kinase) %>%
-    # filter for desired compounds and targets and order by input lists
-    filter(compound %in% compounds) %>%
-    mutate(compound = fct_relevel(compound, compounds)) %>%
-    filter(target %in% targets) %>%
-    mutate(target = fct_relevel(target, targets)) %>%
     # tidy by pivoting duplicates to one measurement per row
-    pivot_longer(cols = c(pct_Inhibition_1, pct_Inhibition_2), names_to = NULL, values_to = "pct_inhibition") %>%
+    pivot_longer(cols = c(pct_inhibition_1, pct_inhibition_2), names_to = NULL, values_to = "pct_inhibition") %>%
     # wrangle: convert conc to log molar and convert percent inhibition to activity
     mutate(log.conc = log10(Compound_Conc_nM/1e9)) %>%
     mutate(activity = 100 - pct_inhibition)
+  # filter for desired compounds and targets, if given, and order by given lists
+  if(filter){
+    if(!missing(compounds)){
+      print("compounds not missing")
+      data <- data %>%
+        filter(compound %in% compounds) %>%
+        mutate(compound = fct_relevel(compound, compounds))
+    }
+    if(!missing(targets)){
+      print("targets not missing")
+      data <- data %>%
+        filter(target %in% targets) %>%
+        mutate(target = fct_relevel(target, targets))
+    }
+  }
+  return(data)
 }
