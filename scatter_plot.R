63246d67a7d60cb1562c67da564133af02360015
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-07 17:55:36 -0800

    Changed to normal errorbar and moved grouping out of scatterplot function

--- scatter_plot.R
+++ scatter_plot.R
@@ -4,16 +4,14 @@ scatter_plot <- function(data,
                          width = 7, height = 7, dodge_width = .2,
                          plot_name = "scatter_plot"){
   data %>%
-    group_by(compound, target) %>%
     inhibition_summarize() %>%
     ggplot(aes(y = target, color = compound)) +
     geom_point(aes(x = mean_pct_inhibition), size = pt_size, alpha = alpha,
                position = position_dodge(width = dodge_width)) +
-    # geom_line(aes(x = mean_pct_inhibition)) +
-    geom_errorbarh(aes(xmax = mean_pct_inhibition+sem,
-                       xmin = mean_pct_inhibition-sem,
-                       height = bar_size),
-                       position = position_dodge(dodge_width),
+    geom_errorbar(aes(xmax = mean_pct_inhibition+sem,
+                      xmin = mean_pct_inhibition-sem,
+                      width = bar_size),
+                  position = position_dodge(width = dodge_width),
                   alpha = alpha) +
     scale_color_viridis(discrete = TRUE, begin = viridis_begin, end = viridis_end) +
     guides(color = guide_legend(override.aes = list(alpha = 1))) + # opaque legend
