9cd90fcd1a061d2661008640f43892d44cb0af49
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-09 19:22:23 -0800

    suppressed EC printing

--- get_EC.R
+++ get_EC.R
@@ -2,6 +2,6 @@ get_EC <- function(data, cpd, tgt, EC_threshold){
   cpd_data <- data %>%
     filter(compound == cpd, target == tgt)
   # fit 4-param logistic model, then extract EC value
-  EC <- ED(drm(activity~log.conc, data=cpd_data, fct=L.4()), EC_threshold)[1,1]
+  EC <- ED(drm(activity~log.conc, data=cpd_data, fct=L.4()), EC_threshold, display = FALSE)[1,1]
   return(EC)
 }
