6b43356caec8c98401b5d088c2816305c609d9c6
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-09 19:15:36 -0800

    Comment for EC fitting

--- get_EC.R
+++ get_EC.R
@@ -1,6 +1,7 @@
 get_EC <- function(data, cpd, tgt, EC_threshold){
   cpd_data <- data %>%
     filter(compound == cpd, target == tgt)
+  # fit 4-param logistic model, then extract EC value
   EC <- ED(drm(activity~log.conc, data=cpd_data, fct=L.4()), EC_threshold)[1,1]
   return(EC)
 }
