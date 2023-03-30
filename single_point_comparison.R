b5d7f68398ffe58a852f4bb61a32c04b2be2dcfd
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-30 14:50:10 -0700

    New script to predict the activities of a set of treatments at the same dose for easy comparison

---
+++ single_point_comparison.R
@@ -0,0 +1,36 @@
+#' ---
+#'title: "single point comparison"
+#'author: "Jack Stevenson"
+#'date: "2023-03-30"
+#' ---
+#'Predict the activity of a set of SelectScreen treatments at the same single dose
+#'- import a SelectScreen dose-response dataset for the treatments of interest
+#'- fit a dose-response model for each treatment
+#'- use the fitted models to predict the activity of each treatment at the given comparison dose
+#'- output a CSV with the results
+# set up------------------------------------------------------------------------
+library(drc)  # for dose response curves
+library(tidyverse) # for tidy data handling
+options(dplyr.summarise.inform = FALSE)
+# import helper functions
+source("import_selectscreen.R")
+source("get_drm.R")
+source("predict_activity.R")
+dir.create("output/", showWarnings = FALSE) # silently create output directory
+# set global variables---------------------------------------------------------
+source("parameters/treatments.R")
+dose_nM <-  1.1 # dose for which to predict activity of treatments
+target <- "ABL1" # target for which to predict activity of treatments
+# vectorized function to predict activity from targets inside mutate()
+predict_targets <- Vectorize(predict_activity, vectorize.args = "trt")
+# import data------------------------------------------------------------------
+input_filename <- "ZLYTE_compiled_results_complete.csv"
+all_data <- import_selectscreen(input_filename, treatments)
+# predict activity and write output--------------------------------------------
+#treatment_predictions <- map(treatments, wrapper_test)
+output <- tibble(treatment = treatments) |>
+  mutate(prediction_dose = dose_nM,
+         #predicted_activity = predict_activity(all_data, treatment = treatment, target = target, dose_nM = dose_nM))
+         name_of_treatment = treatment,
+         predicted_activity = predict_targets(all_data, trt = treatment, tgt = target, dose_nM = dose_nM))
+write_csv(output, "output/predicted_treatment_activity.csv")
