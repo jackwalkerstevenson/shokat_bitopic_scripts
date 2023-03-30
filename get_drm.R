646201211d2b5b85fe6e1d4bbe3c547cc7b22240
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-30 14:14:34 -0700

    expanded variable names in model prediction helper functions for clarity

--- get_drm.R
+++ get_drm.R
@@ -1,7 +1,7 @@
 # get the drc dose-response model for a combination of treatment and target
-get_drm <- function(data, trt, tgt){
+get_drm <- function(data, treatment, target){
   cpd_data <- data %>%
-    filter(treatment == trt, target == tgt)
+    filter(treatment == treatment, target == target)
   # 4-param logistic model on pre-log-transformed data
   return(drm(activity~log.conc, data=cpd_data, fct=L.4()))
 }
