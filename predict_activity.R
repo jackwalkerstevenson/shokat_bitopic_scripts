bf4329adc33bd61cf1ecb2d1d29ad732e85a20c8
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-30 14:40:52 -0700

    Undoing variable name expansion because it actually breaks get_drm

--- predict_activity.R
+++ predict_activity.R
@@ -1,7 +1,7 @@
 # predict the activity of a treatment on a target at a given dose
-predict_activity <- function(data, treatment, target, dose_nM){
+predict_activity <- function(data, trt, tgt, dose_nM){
   log_dose <- log10(dose_nM/1e9)
-  model <- get_drm(data = data, treatment = treatment, target = target)
+  model <- get_drm(data = data, trt = trt, tgt = tgt)
   newdata <- data.frame(log.conc = log_dose)
   return(predict(model, newdata = newdata))
 }
