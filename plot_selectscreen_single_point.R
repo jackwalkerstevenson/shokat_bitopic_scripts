08f5bf73b1f7834936837f56268f06985be854a9
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-30 15:38:29 -0700

    Cleaned up comments a little for selectscreen single point

--- plot_selectscreen_single_point.R
+++ plot_selectscreen_single_point.R
@@ -5,33 +5,33 @@
 #' ---
 #'edited version of plateplotr for dealing with SelectScreen data
 #'started 2023-03-06
-#'# load required libraries-----------------------------------------------------
+# load required libraries-----------------------------------------------------
 library(tidyverse) # for tidy data handling
 library(ggprism)  # for pretty prism-like plots
 library(viridis) # for color schemes
 library(assertthat) # for QC assertions
 options(dplyr.summarise.inform = FALSE)
 # set global variables----------------------------------------------------------
-input_filename <- "ZLYTE_compiled_results_single_point.csv"
-plot_type <- "pdf" # file type for saved output plots
-dir.create("output/", showWarnings = FALSE) # silently create output directory
 source("parameters/treatments.R")
 source("parameters/targets.R")
 source("import_selectscreen.R")
 source("viridis_range.R")
 source("scatter_plot.R")
+dir.create("output/", showWarnings = FALSE) # silently create output directory
+plot_type <- "pdf" # file type for saved output plots
+input_filename <- "ZLYTE_compiled_results_single_point.csv"
 all_data <- import_selectscreen(input_filename, treatments)
 # helper summary function-------------------------------------------------------
 inhibition_summarize <- function(x){
   x %>% group_by(target) %>%
-    summarize(across(c(treatment, pct_inhibition)), # don't drop nongroup variables
-              # scale error bars by group size: dodge issue workaround
+    summarize(across(c(treatment, pct_inhibition)), # keep variables not in current group
+              # scale error bars by size of target group: workaround for dodge issue
               bar_size = .1 * n()) %>%
     group_by(target, treatment) %>%
-    summarize(bar_size, # don't drop nongroup variables
+    summarize(bar_size, # keep variable not in current group
       # standard error for error bars = standard deviation / square root of n
       sem = sd(pct_inhibition, na.rm = TRUE)/sqrt(n()),
-      # get mean value for plotting
+      # mean value for plotting
       mean_pct_inhibition = mean(pct_inhibition),
   )}
 # aesthetic parameters---------------------------------------------------------
