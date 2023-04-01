5c528d295fc7a205a699b6117176f0ce92d8db3a
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-31 20:04:52 -0700

    Moved name of EC50 prediction function

--- predict_EC50_nM_from_point.R
+++ predict_EC50_nM_from_point.R
@@ -7,17 +7,19 @@
 #'parameters:
 #'- logmolar_dose: dose (single or vector) in logmolar units (1 nM = -9 logmolar)
 #'- pct_inhibition: percent inhibition (single or vector) corresponding to doses
-predict_EC50_from_point <- function(logmolar_dose, pct_inhibition, fixed_params){
+predict_EC50_nM_from_point <- function(logmolar_dose, pct_inhibition, fixed_params){
   # assertthat::assert_that(length(logmolar_dose) == length(pct_inhibition))
   # convert pct inhibition to activity, since prefit model is activity~log.conc
   act <- 100 - pct_inhibition
+  print(str_glue("predicting EC50 from {pct_inhibition}% inhibition at logmolar conc {logmolar_dose}"))
   x <- logmolar_dose
   y <- act
   b <- fixed_params["hill_slope"]
   c <- fixed_params["max_effect"]
   d <- fixed_params["min_effect"]
   # rearranged 4-param logistic to solve for EC50
-  log_EC50 <- x/(((d-y)/(y-c))^(1/b))
-  print(str_glue("found log EC50 {log_EC50}"))
-  return(10^log_EC50*1e9) # convert EC50 to nM
+  # -b hacked in for negative hill slope
+  EC50_log <- unname(x/(((d-y)/(y-c))^(1/-b)))
+  EC50_nM <- 10^EC50_log*1e9
+  return(EC50_nM) # convert EC50 to nM
 }
