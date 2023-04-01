19e6629c2769bf4949aa9b34b5363d58c9a6fcc6
jackwalkerstevenson <jackwalkerstevenson@gmail.com>
2023-03-31 20:04:34 -0700

    Added additional function to fit a drm on percent inhibition rather than activity

--- get_drm.R
+++ get_drm.R
@@ -5,3 +5,11 @@ get_drm <- function(data, trt, tgt){
   # 4-param logistic model on pre-log-transformed data
   return(drm(activity~log.conc, data = data_subset, fct = L.4()))
 }
+
+# fit a drc dose-response model for a treatment on a target
+get_drm_pct <- function(data, trt, tgt){
+  data_subset <- data %>%
+    filter(treatment == trt, target == tgt)
+  # 4-param logistic model on pre-log-transformed data
+  return(drm(pct_inhibition~log.conc, data = data_subset, fct = L.4()))
+}
