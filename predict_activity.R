c093f7cae37f892ddc9a74877eb5d519b300e4aa
Jack Stevenson <jackstevenson@Jacks-MacBook-Pro.local>
2023-04-28 18:26:11 -0700

    Changed plateplotr predict activity code to indicate legacy. Added "drm" to function name and indicated that get_drm function is legacy plateplotr. This function will be fully deprecated

--- predict_activity.R
+++ predict_activity.R
@@ -1,8 +1,9 @@
 # predict the activity of a treatment on a target at a given dose
-predict_activity <- function(data, trt, tgt, dose_nM){
+predict_activity_drm <- function(data, trt, tgt, dose_nM){
   # models are fitted on log molar conc, so have to convert the desired nM conc
-  log_dose <- log10(dose_nM/1e9)
-  model <- get_drm(data = data, trt = trt, tgt = tgt)
+  log_dose <- nM_to_logM(dose_nM)
+  model <- get_drm_plateplotr(data = data, trt = trt, tgt = tgt)
+  print(model)
   # yucky syntax to get predict.drc to predict activity at given value of log.conc
   newdata <- data.frame(log.conc = log_dose)
   return(predict(model, newdata = newdata))
