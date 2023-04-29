812542eb169eb0d20f48e2d962ef0c53f95690c7
Jack Stevenson <jackstevenson@Jacks-MacBook-Pro.local>
2023-04-28 18:28:30 -0700

    Moved viridis_range to legacy viridis_range_plateplotr name. Will be fully deprecated and replaced by viridis_range() from doseplotr

--- viridis_range_plateplotr.R
+++ viridis_range_plateplotr.R
@@ -1,7 +1,7 @@
 # pick a visually pleasing range of the default viridis color palette for discrete values
 # returns c(start, end) values
 # 2 values should be 0.4ish
-viridis_range <- function(n, reverse = TRUE){
+viridis_range_plateplotr <- function(n, reverse = TRUE){
   range <- ifelse(n < 6, 0.2*n, 1)
   high <- 0.5 + range / 2
   low <- 0.5 - range / 2
