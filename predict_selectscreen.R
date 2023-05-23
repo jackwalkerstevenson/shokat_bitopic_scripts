542add3326a69f4d0e0e1b353d13a1e088a3ac45
Jack Stevenson <2307047+jackwalkerstevenson@users.noreply.github.com>
2023-05-22 18:25:09 -0700

    Improved selectscreen prediction to run multiple models for all targets

--- predict_selectscreen.R
+++ predict_selectscreen.R
@@ -2,7 +2,10 @@
 # created 2023-05-22
 library(dplyr)
 library(doseplotr)
-SS_data <- import_selectscreen("ZLYTE_compiled_results_complete.csv")
+SS_curve_filename <- "ZLYTE_compiled_results_complete.csv"
+SS_single_pt_filename <- "ZLYTE_compiled_results_single_point.csv"
+SS_data <- import_selectscreen(SS_curve_filename)
+pt_data <- import_selectscreen(SS_single_pt_filename)
 P12_ABL1_SS_model <- SS_data |>
   filter_trt_tgt("PonatiLink-2-7-12", "ABL1") |> 
   get_drda()
@@ -31,3 +34,18 @@ est_SS_EC50_P12_ABL1 <- function(conc_nM, pct_inhibition){
 est_SS_EC50_P12_FGFR1 <- function(conc_nM, pct_inhibition){
   est_SS_EC50(P12_FGFR1_SS_model, conc_nM, pct_inhibition)
 }
+# predict EC50s for PL-2-7-10 on all single-pt kinases using both models
+predict_all_EC50s_from_points <- function(trt){
+  pt_data |> 
+    filter(treatment == trt) |> 
+    group_by(target) |>
+    summarize(conc_nM = mean(Compound.Conc), # should only be one conc
+              mean_inhib = mean(pct_inhibition)) |> 
+    mutate(EC50_pred_P12_ABL1 = est_SS_EC50_P12_ABL1(conc_nM, mean_inhib),
+           EC50_pred_P12_FGFR1 = est_SS_EC50_P12_FGFR1(conc_nM, mean_inhib),
+           disregard = mean_inhib < 5 | mean_inhib > 95)
+}
+
+pt_EC50_predictions_P10 <- predict_all_EC50s_from_points("PonatiLink-2-7-10")
+pt_EC50_predictions_PA <- predict_all_EC50s_from_points("ponatinib + asciminib")
+pt_EC50_predictions_P24 <- predict_all_EC50s_from_points("PonatiLink-1-24")
