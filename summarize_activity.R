842d21120112e9552c68a25676dbf3dad7feb363
Jack Stevenson <2307047+jackwalkerstevenson@users.noreply.github.com>
2023-05-27 11:02:48 -0700

    Changed activity to response_norm in summarize_activity

--- summarize_activity.R
+++ summarize_activity.R
@@ -2,11 +2,11 @@
 summarize_activity <- function(x){
   summarize(x,
             # standard error for error bars = standard deviation / square root of n
-            confidence_intervals = list(mean_cl_normal(activity) |>
+            confidence_intervals = list(mean_cl_normal(response_norm) |>
                         rename(mean_activity = y, ymin_activity = ymin, ymax_activity = ymax)),
-            sem = sd(activity, na.rm = TRUE)/sqrt(n()),
+            sem = sd(response_norm, na.rm = TRUE)/sqrt(n()),
             # get mean normalized readout value for plotting
-            mean_read = mean(activity),
+            mean_read = mean(response_norm),
             w = 0.06 * n() # necessary for consistent error bar widths across plots
   ) |>
     tidyr::unnest(cols = confidence_intervals)
