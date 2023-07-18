3f27d3bb1be19bdffc418f1e3d1e839f08ccee42
Jack Stevenson <2307047+jackwalkerstevenson@users.noreply.github.com>
2023-07-17 17:45:28 -0700

    pipe update in plot_EC50

--- plot_EC50.R
+++ plot_EC50.R
@@ -22,19 +22,19 @@ source("parameters/treatments.R")
 source("parameters/targets.R")
 source("parameters/variants.R")
 source("parameters/keys.R")
-EC_data <- read_csv(input_filename) %>%
-  mutate(linker_length = as.numeric(linker_length)) %>%
-  mutate(EC50_nM = as.numeric(EC50_nM)) %>%
+EC_data <- read_csv(input_filename) |>
+  mutate(linker_length = as.numeric(linker_length)) |>
+  mutate(EC50_nM = as.numeric(EC50_nM)) |>
   # filter for desired treatments, targets and target variants
-  filter(treatment %in% treatments) %>%
-  filter(target %in% targets) %>%
-  filter(variant %in% variants) %>%
-  mutate(treatment = fct_relevel(treatment, treatments)) %>% # order treatments by list
-  mutate(target = fct_relevel(target, targets)) %>% # order targets by list
-  mutate(variant = fct_relevel(variant, variants)) %>% # order variants by list
+  filter(treatment %in% treatments) |>
+  filter(target %in% targets) |>
+  filter(variant %in% variants) |>
+  mutate(treatment = fct_relevel(treatment, treatments)) |> # order treatments by list
+  mutate(target = fct_relevel(target, targets)) |> # order targets by list
+  mutate(variant = fct_relevel(variant, variants)) |> # order variants by list
   mutate(neglog10EC50_nM = -log10(EC50_nM))
 # plot ECs in points--------- ------------------------------------------------------
-EC_data %>%
+EC_data |>
   ggplot(aes(x = treatment, y = EC50_nM)) +
   geom_point(aes(shape = assay, color = variant), size = 4, alpha = 1) +
   scale_shape(labels = assay_labels,
@@ -53,16 +53,16 @@ ggsave(str_glue("output/EC50_points_{get_timestamp()}.{plot_type}"),
           width = 8,
           height = 6)
 # calculate linker length scale parameters--------------------------------------
-linkers <- EC_data %>%
-  filter(linker_length > 0) %>%
-  distinct(linker_length) %>%
+linkers <- EC_data |>
+  filter(linker_length > 0) |>
+  distinct(linker_length) |>
   pull(linker_length)
 linker_min <- min(linkers)
 linker_max <- max(linkers)
 linker_seq <- seq(linker_min, linker_max, 2)
 # plot ECs by linker length-----------------------------------------------------
-EC_data %>%
-  filter(linker_length > 0) %>% # only plot treatments with linkers
+EC_data |>
+  filter(linker_length > 0) |> # only plot treatments with linkers
   ggplot(aes(x = linker_length, y = EC50_nM,
              shape = assay, color = variant)) +
   scale_shape(labels = assay_labels,
@@ -87,12 +87,12 @@ ggsave(str_glue("output/EC50_linker_{get_timestamp()}.{plot_type}"),
        width = 8,
        height = 4)
 # plot ECs across assays--------------------------------------------------------
-EC_data %>%
+EC_data |>
   # pivot so EC50s from both assays associate with each linker/variant combo
   pivot_wider(names_from = assay,
               values_from = EC50_nM,
-              id_cols = c(linker_length, variant)) %>%
-  filter(linker_length > 0) %>% # only plot treatments with linkers
+              id_cols = c(linker_length, variant)) |>
+  filter(linker_length > 0) |> # only plot treatments with linkers
   ggplot(aes(y = CTG, x = SelectScreen)) +
   theme_prism() + # make it look fancy like prism
   lemon::facet_rep_wrap(vars(variant), repeat.tick.labels = "left") + # repeat y axis
