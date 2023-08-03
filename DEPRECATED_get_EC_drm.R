7c3ffb6121efee0cbeb88df97a0258d03b7ba35d
Jack Stevenson <2307047+jackwalkerstevenson@users.noreply.github.com>
2023-08-03 16:54:26 -0700

    Removed deprecated old drm scripts

--- DEPRECATED_get_EC_drm.R
+++
@@ -1,8 +0,0 @@
-get_EC_nM_drm <- function(data, trt, tgt, EC_threshold){
-  cpd_data <- data %>%
-    filter(treatment == trt, target == tgt)
-  # fit 4-param logistic model, then extract EC value
-  EC_log <- ED(drm(activity~conc_logM, data=cpd_data, fct=L.4()),
-           EC_threshold, display = FALSE)[1,1]
-  return(10^EC_log*1e9) # 1e9 for nM
-}
--- DEPRECATED_get_hill_slope_drm.R
+++
@@ -1,6 +0,0 @@
-get_hill_slope_drm <- function(data, trt, tgt){
-  cpd_data <- data %>%
-    filter(treatment == trt, target == tgt)
-  slope <- coef(drm(activity~conc_logM, data=cpd_data, fct=L.4()))[1]
-  return(slope)
-}
