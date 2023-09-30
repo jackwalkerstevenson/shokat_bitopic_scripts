bd914376411bb77994a109a10022860856c95fe0
Jack Stevenson <2307047+jackwalkerstevenson@users.noreply.github.com>
2023-09-29 18:36:38 -0700

    Updated plot_kinomescan logic

--- plot_kinomescan.R
+++ plot_kinomescan.R
@@ -15,116 +15,118 @@ library(patchwork) # for plot organization
 library(assertthat) # for QC assertions
 library(doseplotr) # you bet
 options(dplyr.summarise.inform = FALSE)
-# set global variables---------------------------------------------------------
-input_filename <- "2023-06-27 kinomescan raw data edited for import.csv"
-plot_type <- "pdf" # file type for saved output plots
-dir.create("output/", showWarnings = FALSE) # silently create output directory
-source("parameters/treatments.R")
-source("parameters/targets.R")
+# import global parameters and clear environment--------------------------------
+rm(list = ls()) # clear environment
+source("parameters/parameters_plot_kinomescan.R")
 # import and tidy data---------------------------------
 all_data <- import_kinomescan(str_glue("input/{input_filename}")) |>
   filter(log_dose != -Inf) |> # drop 0 values
   filter_trt_tgt(treatments, targets) |> 
   mutate(treatment = fct_relevel(treatment, treatments),
          target = fct_relevel(target, targets),)
-# fit models to output EC values------------------------------------------------
-EC_summary <- summarize_models(all_data, activity_col = "response_norm")
-write_csv(EC_summary, str_glue("output/kinomescan_model_summary_{get_timestamp()}.csv"))
-# generate global parameters for all plots------------------------------------------
-pt_size = 3 # size for all geom_point
+# filter and validate imported data---------------------------------------------
+if(exists("treatments")){
+  all_data <- filter_validate_reorder(all_data, "treatment", treatments)
+}
+if(exists("targets")){
+  all_data <- filter_validate_reorder(all_data, "target", targets)
+}
+plot_data <- all_data |> filter(log_dose != -Inf) # data without untreated
+# generate data-dependent global plot parameters--------------------------------
+if (!exists("treatments")){ # if treatments not specified, use all treatments
+  treatments <- as.vector(unique(plot_data$treatment))}
+if (!exists("targets")){ # if targets not specified, use all targets
+  targets <- as.vector(unique(plot_data$target))}
 # find x-axis min/max values for consistent zoom window between all plots
-x_min <- floor(min(all_data$log_dose))
-x_max <- ceiling(max(all_data$log_dose))
-x_limits <- c(x_min, x_max)
-# create logistic minor breaks for all conc plots
-minor_x <- log10(rep(1:9, x_max - x_min)*(10^rep(x_min:(x_max - 1), each = 9)))
-# helper function to add ggplot objects common to all plots--------------------
-source("dose_response_global.R")
-# helper function to plot one treatment----------------------------------------
-plot_treatment <- function(data, trt,
-                           viridis_begin = 1, viridis_end = 0, viridis_option = "viridis"){
-  plate_summary <- data |> 
-    filter(treatment == trt) |>  # get data from one treatment to work with
-    group_by(target, log_dose) |>   # get set of replicates for each condition
-    summarize_response(response_col = "response_norm")
-  # bracket ggplot so it can be piped to helper function
-  {ggplot(plate_summary, aes(x = log_dose, y = mean_response, color = target)) +
-      geom_point(aes(shape = target), size = pt_size) +
-      # error bars = mean plus or minus standard error
-      geom_errorbar(aes(ymax = mean_response+sem, ymin = mean_response-sem, width = w)) +
-      # use drm method from drc package to fit dose response curve
-      #L.4 method is 4-param logistic curve. The more common LL.4() works on nonlog
-      geom_line(stat = "smooth", method = "drm", method.args = list(fct = L.4()),
-                se = FALSE, linewidth = 1)} |>
-    dose_response_global(x_limits) +
-    theme(aspect.ratio = 1) +
-    scale_color_viridis(discrete = TRUE, begin = viridis_begin, end = viridis_end) +
-    labs(title = trt,
-         y = "response")
+if(override_x_lim){
+  x_limits <- manual_x_lim
+} else{
+  x_min <- floor(min(plot_data$log_dose))
+  x_max <- ceiling(max(plot_data$log_dose))
+  x_limits <- c(x_min, x_max)
 }
-# plot data for each treatment separately----------------------------------------
+# x_limits <- c(-12,-4) # manual x limit backup
+# fit models and report model parameters----------------------------------------
+model_summary <- summarize_models(plot_data,
+                                  response_col = "response_norm",
+                                  rigid = rigid) |> # use global rigid parameter
+  dplyr::select(-model) |>  # remove actual model from report
+  mutate(across(where(is.numeric), \(x){signif(x, digits = 4)}))
+write_csv(model_summary,
+          str_glue("output/kinomescan_model_summary_{get_timestamp()}.csv"))
+# plot data for each treatment separately---------------------------------------
+target_legend_title <- if(override_target_title){
+  target_title} else "cell line"
 for (trt in treatments){
-  trt_data <- filter_trt_tgt(all_data, trt)
-  num_tgts <- length(unique(trt_data$target))
-  vr <- viridis_range(num_tgts)
-  vr_begin <- vr[[1]]
-  vr_end <- vr[[2]]
-  vr_option <- vr[[3]]
-  p <- plot_treatment(trt_data, trt, viridis_option = vr_option,
-                      viridis_begin = vr_begin, viridis_end = vr_end)
-  # save plot with manually optimized aspect ratio
-  save_plot(p, str_glue("output/kinomescan_treatment_{trt}_{get_timestamp()}.{plot_type}"),
-            legend_len = longest(targets))
+  # get all targets for this treatment to set legend length
+  trt_targets <- as.vector(unique((plot_data |>
+                                     filter_trt_tgt(trt = trt))$target))
+  if(manually_relabel_targets){
+    trt_targets <- Vectorize(get_display_name, vectorize.args = "name")(
+      trt_targets, display_names_targets, TRUE)}
+  plot_treatment(plot_data, trt,
+                 rigid = rigid, # global param: rigid low-dose asymptote
+                 grid = grid, # global param: background grid on plot
+                 no_legend = no_legend, # global param: whether to omit legend
+                 x_limits = get_if(x_limits, global_x_lim),
+                 response_col = "response_norm",
+                 ylab = "signal (% of untreated)",
+                 legend_title = target_legend_title,
+                 # if relabeling targets, get display names for legend
+                 legend_labels = get_if(display_names_targets,
+                                        manually_relabel_targets,
+                                        otherwise = ggplot2::waiver()),
+                 #if relabeling, get display name for title
+                 plot_title = get_display_name(trt,
+                                               display_names_treatments,
+                                               manually_relabel_treatments),
+                 # if manually setting colors of targets, get color map
+                 color_map = get_if(color_map_targets,
+                                    manually_recolor_targets),
+                 # if manually setting shapes of targets, get shape map
+                 shape_map = get_if(shape_map_targets,
+                                    manually_reshape_targets)
+  ) |> 
+    save_plot(
+      str_glue("output/kinomescan_treatment_{trt}_{get_timestamp()}.{plot_type}"),
+      legend_len = if(no_legend) 0 else{
+        longest(c(trt_targets, target_legend_title))})
 }  
-# set color parameters for target plots--------------------------------------
-vr <- viridis_range(length(treatments))
-vr_begin <- vr[[1]]
-vr_end <- vr[[2]]
-vr_option <- vr[[3]]
 # plot data for each target separately------------------------------------------
-for (tgt in targets){
-  plate_summary <- all_data |>
-    filter(target == tgt) |>
-    group_by(treatment, log_dose) |> # group into replicates for each condition
-    summarize_response(response_col = "response_norm")
-  # bracket ggplot so it can be piped to helper function
-  p <- {ggplot(plate_summary, aes(x = log_dose, y = mean_response, color = treatment)) +
-      geom_point(aes(shape = treatment), size = pt_size) +
-      # error bars = mean plus or minus standard error
-      geom_errorbar(aes(ymax = mean_response+sem, ymin = mean_response-sem,
-                        width = w),
-                    alpha = alpha_val) +
-      # use drm method from drc package to fit dose response curve
-      geom_line(stat = "smooth", method = "drm", method.args = list(fct = L.4()),
-                se = FALSE, linewidth = 1, alpha = alpha_val)} |>
-    dose_response_global(x_limits) +
-    scale_color_viridis(discrete = TRUE, option = vr_option,
-                        begin = vr_begin, end = vr_end) +
-    labs(title = tgt,
-         y = "kinase activity (%)")
-  save_plot(p, str_glue("output/kinomescan_target_{tgt}_{get_timestamp()}.{plot_type}"),
-            legend_len = longest(treatments))
+treatment_legend_title <- if(override_treatment_title){
+  treatment_title} else "treatment"
+for (tgt in targets){ 
+  # get all treatments for this target to set legend length
+  tgt_treatments <- as.vector(unique((plot_data |>
+                                        filter_trt_tgt(tgt = tgt))$treatment))
+  if(manually_relabel_treatments){
+    tgt_treatments <- Vectorize(get_display_name, vectorize.args = "name")(
+      tgt_treatments, display_names_treatments, TRUE)}
+  plot_target(plot_data, tgt,
+              rigid = rigid, # global param: rigid low-dose asymptote
+              grid = grid, # global param: background grid on plot
+              no_legend = no_legend, # global param: whether to omit legend
+              x_limits = get_if(x_limits, global_x_lim),
+              response_col = "response_norm",
+              ylab = "signal (% of untreated)",
+              legend_title = treatment_legend_title,
+              # if relabeling treatments, get display names for legend
+              legend_labels = get_if(display_names_treatments,
+                                     manually_relabel_treatments,
+                                     otherwise = ggplot2::waiver()),
+              #if relabeling, get display name for title
+              plot_title = get_display_name(tgt,
+                                            display_names_targets,
+                                            manually_relabel_targets),
+              # if manually setting colors of treatments, get color map
+              color_map = get_if(color_map_treatments,
+                                 manually_recolor_treatments),
+              # if manually setting shapes of treatments, get shape map
+              shape_map = get_if(shape_map_treatments,
+                                 manually_reshape_treatments)
+  )|>
+    save_plot(
+      str_glue("output/kinomescan_target_{tgt}_{get_timestamp()}.{plot_type}"),
+      no_legend = no_legend,
+      legend_len = longest(c(tgt_treatments, treatment_legend_title)))
 }
-# plot data for all targets at once-----------------------------------------
-vr <- viridis_range(length(treatments))
-vr_begin <- vr[[1]]
-vr_end <- vr[[2]]
-vr_option <- vr[[3]]
-plate_summary <- all_data |>
-  group_by(target, treatment, log_dose) |> # group into replicates for each condition
-  summarize_response(response_col = "response_norm")
-
-p <- {ggplot(plate_summary,aes(x = log_dose, y = mean_response, color = treatment)) +
-    geom_point(aes(shape = treatment), size = pt_size) +
-    # error bars = mean plus or minus standard error
-    geom_errorbar(aes(ymax = mean_response+sem, ymin = mean_response-sem, width = w), alpha = alpha_val) +
-    # use drm method from drc package to fit dose response curve
-    geom_line(aes(linetype = target), stat = "smooth", method = "drm", method.args = list(fct = L.4()),
-              se = FALSE, linewidth = 1, alpha = alpha_val)} |>
-  dose_response_global(x_limits) +
-  scale_color_viridis(option = vr_option, discrete = TRUE,
-                      begin = vr_begin, end = vr_end) +
-  labs(title = "All data",
-       y = "kinase activity (%)")
-save_plot(p, str_glue("output/kinomescan_all_data_{get_timestamp()}.{plot_type}"),
-          legend_len = longest(treatments))
