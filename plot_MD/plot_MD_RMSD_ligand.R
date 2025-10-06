7c845be18320343bb08970a3a1d873e56bf7a608
Jack Stevenson <2307047+jackwalkerstevenson@users.noreply.github.com>
2025-10-06 09:08:15 -0400

    plot_MD_RMSD updated to downsample and cut columns from data and then write downsampled raw output

--- plot_MD/plot_MD_RMSD_ligand.R
+++ plot_MD/plot_MD_RMSD_ligand.R
@@ -41,6 +41,15 @@ dat_files <- list.files(path = input_dir, pattern = "*_rms\\.lig\\.dat$", full.n
 all_data <- dat_files |> 
   purrr::map_dfr(read_rms_file) |> 
   left_join(key_data, by = join_by(kenneth_id))
+
+downsample_factor <- 20 # integer factor by which to downsample
+# report processed data, downsampled
+downsampled_data <- all_data |> 
+  dplyr::select(frame, rmsd, kenneth_id, run, time_ns, compound_name_full) |> 
+  dplyr::filter(frame %% downsample_factor == 0)
+write_csv(downsampled_data,
+          fs::path(output_dir,
+                   str_glue("MD_RMSD_ligand_all_data_{get_timestamp()}.csv")))
 # count and report number of frames for each run--------------------------------
 frame_summary <- all_data |>
   dplyr::count(kenneth_id, run)
@@ -48,10 +57,7 @@ write_csv(frame_summary,
           fs::path(output_dir,
                    str_glue("MD_RMSD_ligand_frames_{get_timestamp()}.csv")))
 # plot RMSD by compound_name_full and run-----------------------------------------------
-sparse_sample_factor <- 20
-all_data |>
-  # sparse sampling
-  dplyr::filter(frame %% sparse_sample_factor == 0) |> 
+downsampled_data |>
   ggplot(aes(x = time_ns, y = rmsd, color = compound_name_full, linetype = run)) +
   # geom_line(alpha = 0.4) +
   geom_smooth(alpha = 0.7) +
